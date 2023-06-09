bc1780211b61868059f5ff6acf6d446f04484386
Daniel O'Connell <github@ahiru.pl>
2023-06-10 00:12:18 +0200

    Update stampy code

--- align_data/stampy/stampy.py
+++ align_data/stampy/stampy.py
@@ -1,46 +1,51 @@
+import os
+import logging
 from dataclasses import dataclass
 import requests
-import logging
-from align_data.common.alignment_dataset import AlignmentDataset , DataEntry
-from tqdm import tqdm
+from dotenv import load_dotenv
+from codaio import Coda, Document
+from align_data.common.alignment_dataset import AlignmentDataset, DataEntry
+
+load_dotenv()
+CODA_TOKEN = os.environ.get("CODA_TOKEN", "token not found")
 
 logger = logging.getLogger(__name__)
+
 
 @dataclass
 class Stampy(AlignmentDataset):
 
-    index_url : str
-    done_key = "entry"
+    coda_doc_id : str
+    on_site_table : str
+    done_key = "question"
 
     @property
     def items_list(self):
-        entries = dict(requests.get(self.index_url).json())
-        return entries["results"].keys()
+        coda = Coda(CODA_TOKEN)
+        doc = Document(self.coda_doc_id, coda=coda)
+        logger.info('Fetching table: %s', self.coda_doc_id)
+        table = doc.get_table(self.on_site_table)
+        return table.to_dict() # a list of dicts
 
-    def get_item_key(self, item):
-        return item
+    def get_item_key(self, entry):
+        return entry.get('Question', '')
 
     def process_entry(self, entry):
-        qa_entry = entries["results"][entry]
-        qa_entry["question"] = ' '.join(entry.split("to ")[1:])
-        qa_entry["answer"] = entries["results"][entry]["printouts"]["Answer"]
-        qa_entry["text"] = "Question: " + qa_entry["question"] + "\n\nAnswer: " + entries["results"][entry]["printouts"]["Answer"][0]
-        # if there is more than one answer, add the rest
-        for jj in range(1, len(entries["results"][entry]["printouts"]["Answer"])):
-            qa_entry["text"] += f"\n\nAnswer {str(jj)}: " + entries["results"][entry]["printouts"]["Answer"][jj]
+        question = entry.get('Question', '')
+        answer = entry.get('Rich Text', '')  # Assuming 'Answer' is present in each dict
+        url = entry.get('Link', '')
+        date_published = entry.get('Doc Last Edited', '')
 
-
-        logger.info(f"Processing {ii}")
+        logger.info(f"Processing {question}")
 
         return DataEntry({
-            "source" : self.name,
+            "source": self.name,
             "source_filetype": "text",
-            "url": "n/a",
-            "title": qa_entry["question"],
+            "url": url,
+            "title": question,
             "authors": "n/a",
-            "date_published": "n/a",
-            "text": qa_entry["text"],
-            "question": qa_entry["question"],
-            "answer": qa_entry["answer"],
-            "entry": entry
+            "date_published": date_published,
+            "text": answer,
+            "question": question,
+            "answer": answer,
         })
