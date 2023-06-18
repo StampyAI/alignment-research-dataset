6336bbfa20c077bc6a4ddc27fa0aeabce94bb601
Daniel O'Connell <github@ahiru.pl>
2023-06-18 22:18:32 +0200

    handle empty values in alignment newsletter

--- align_data/alignment_newsletter/alignment_newsletter.py
+++ align_data/alignment_newsletter/alignment_newsletter.py
@@ -33,21 +33,31 @@ class AlignmentNewsletter(AlignmentDataset):
         converted_with, source_type, venue, newsletter_category, highlight, newsletter_number,
         summarizer, opinion, prerequisites, read_more, title, authors, date_published, text
         """
+        if not row.Summary or pd.isna(row.Summary):
+            return None
+
+        def handle_na(v, cast=None):
+            if not v or pd.isna(v):
+                return None
+            if cast:
+                return cast(v)
+            return v
+
         return DataEntry({
-            "url": row.URL or 'n/a',
-            "source": self.name,
+            "url": handle_na(row.URL) or 'n/a',
+            "source": handle_na(self.name),
             "converted_with": "python",
             "source_type": "google-sheets",
-            "venue": str(row.Venue),  # arXiv, Distill, LessWrong, Alignment Forum, ICML 2018, etc
-            "newsletter_category": str(row.Category),
+            "venue": handle_na(row.Venue, str),  # arXiv, Distill, LessWrong, Alignment Forum, ICML 2018, etc
+            "newsletter_category": handle_na(row.Category, str),
             "highlight": row[2] == "Highlight",
-            "newsletter_number": str(row.Email),
-            "summarizer": str(row.Summarizer),
-            "opinion": str(row[11]),
-            "prerequisites": str(row.Prerequisites),
-            "read_more": str(row[13]),
-            "title": str(row.Title),
+            "newsletter_number": handle_na(row.Email, str),
+            "summarizer": handle_na(row.Summarizer, str),
+            "opinion": handle_na(row[11], str),
+            "prerequisites": handle_na(row.Prerequisites, str),
+            "read_more": handle_na(row[13], str),
+            "title": handle_na(row.Title, str),
             "authors": [i.strip() for i in str(row.Authors).split(',')],
-            "date_published": 'n/a' if pd.isna(row.Year) else str(int(row.Year)),
-            "text": str(row.Summary),
+            "date_published": str(handle_na(row.Year, int) or 'n/a'),
+            "text": handle_na(row.Summary, str),
         })
