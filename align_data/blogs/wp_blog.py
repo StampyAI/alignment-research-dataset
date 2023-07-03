54254f9cd40f2e3f129917bc77d1fabf4b9e5b4e
Thomas Lemoine <lemoine123thomas@gmail.com>
2023-07-02 23:43:24 -0400

    added feature

--- align_data/blogs/wp_blog.py
+++ align_data/blogs/wp_blog.py
@@ -37,7 +37,46 @@ class WordpressBlog(AlignmentDataset):
 
     @property
     def items_list(self):
-        return [f"{self.feed_url}?paged={page + 1}" for page in range(0, self.max_pages)]
+        """ 
+        Instead of creating a list of urls up to 2000, 
+        which makes tqdm think that there are 2000 items 
+        to process, we'll find the last valid page number 
+        and create a list of urls up to that page number.
+        We use exponential search to find the last valid page number.
+        """
+        last_title = [None]  # We'll use a list so that we can modify it inside the inner function
+
+        def is_valid_page(i):
+            paged_url = f"{self.feed_url}?paged={i + 1}"
+            d = feedparser.parse(paged_url)
+            if ("feed" in d) and ("title" in d["feed"]) and d["feed"]["title"] != last_title[0]:
+                last_title[0] = d["feed"]["title"]
+                return True
+            else:
+                return False
+
+        pages_url = []
+        bound = 1
+        while is_valid_page(bound):
+            bound *= 2  # Exponentially increase the bound
+            logger.info('bound', bound)
+
+        # Now we know the page number lies between bound/2 and bound, perform binary search
+        lower_bound = bound // 2
+        upper_bound = bound
+        while upper_bound - lower_bound > 1:
+            mid = (lower_bound + upper_bound) // 2
+            if is_valid_page(mid):
+                lower_bound = mid
+            else:
+                upper_bound = mid
+            logger.info('lower_bound', lower_bound, 'upper_bound', upper_bound)
+
+        # add the valid pages to pages_url
+        for i in range(lower_bound + 1):  # Adding 1 to include the last valid page
+            pages_url.append(f"{self.feed_url}?paged={i + 1}")
+
+        return pages_url
 
     @staticmethod
     def _get_published_date(item):
@@ -48,21 +87,9 @@ class WordpressBlog(AlignmentDataset):
         return dt.strftime("%Y-%m-%dT%H:%M:%SZ")
 
     def fetch_entries(self):
-        last_title = ""
         for paged_url in self.unprocessed_items():
             logger.info(f"Fetching {paged_url} (max={self.max_pages})")
             d = feedparser.parse(paged_url)
-
-            if (
-                ("feed" not in d)
-                or ("title" not in d["feed"])
-                or (d["feed"]["title"] == last_title)
-            ):
-                logger.info(
-                    "Not a valid page. It looks like we've reached the end.")
-                break
-
-            last_title = d["feed"]["title"]
 
             for entry in d["entries"]:
                 content_text = self.cleaner.clean(entry["content"][0]["value"])
