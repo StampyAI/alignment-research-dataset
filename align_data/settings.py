b3a73e494d88f71b56288bc13f2debaf655f8ab3
Daniel O'Connell <github@ahiru.pl>
2023-07-04 10:06:49 +0200

    env settings for sheet ids

--- align_data/settings.py
+++ align_data/settings.py
@@ -8,3 +8,7 @@ CODA_DOC_ID = os.environ.get("CODA_DOC_ID", "fau7sl2hmG")
 ON_SITE_TABLE = os.environ.get('CODA_ON_SITE_TABLE', 'table-aOTSHIz_mN')
 
 PDFS_FOLDER_ID = os.environ.get('PDF_FOLDER_ID', '1etWiXPRl0QqdgYzivVIj6wCv9xj5VYoN')
+
+METADATA_SOURCE_SPREADSHEET = os.environ.get('METADATA_SOURCE_SPREADSHEET', '1pgG3HzercOhf4gniaqp3tBc3uvZnHpPhXErwHcthmbI')
+METADATA_SOURCE_SHEET = os.environ.get('METADATA_SOURCE_SHEET', 'special_docs.csv')
+METADATA_OUTPUT_SPREADSHEET = os.environ.get('METADATA_OUTPUT_SPREADSHEET', '1l3azVJVukGAvZPgg0GyeqiaQe8bEMZvycBJaA8cRXf4')
