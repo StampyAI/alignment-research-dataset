f5f8e16542d5d525276aa722b854bca767dac42d
Thomas Lemoine <lemoine123thomas@gmail.com>
2023-07-12 14:01:20 -0400

    fix bug

--- align_data/blogs/wp_blog.py
+++ align_data/blogs/wp_blog.py
@@ -80,7 +80,7 @@ class WordpressBlog(AlignmentDataset):
             content_text = markdownify(entry["content"][0]["value"]).strip()
             text = entry["title"] + "\n\n" + content_text
             
-            new_entry = DataEntry({
+            new_entry = self.make_data_entry({
                 "text": text,
                 "url": entry['link'],
                 "title": text.split("\n")[0],
