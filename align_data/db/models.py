0ca7563ca8c5fb2e7b236402de352a1bf97cab67
Henri Lemoine <henri123lemoine@gmail.com>
2023-08-04 12:41:30 -0400

    fixed mysql db models

--- align_data/db/models.py
+++ align_data/db/models.py
@@ -5,7 +5,7 @@ import hashlib
 from datetime import datetime
 from typing import List, Optional
 from sqlalchemy import JSON, DateTime, ForeignKey, String, Boolean, Text, Float, func, event
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.orm import DeclarativeBase, Mapped, Session, mapped_column, relationship
 from sqlalchemy.dialects.mysql import LONGTEXT
 
 
@@ -29,18 +29,6 @@ class Summary(Base):
     article: Mapped["Article"] = relationship(back_populates="summaries")
 
 
-class Pinecone(Base):
-
-    __tablename__ = "pinecone"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    article_id: Mapped[str] = mapped_column(ForeignKey("articles.id"))
-    update_required: Mapped[bool] = mapped_column(Boolean, default=False)
-    delete_required: Mapped[bool] = mapped_column(Boolean, default=False)
-
-    article: Mapped["Article"] = relationship("Article", back_populates="pinecone")
-
-
 class Article(Base):
     
     __tablename__ = "articles"
@@ -53,14 +41,17 @@ class Article(Base):
     source_type: Mapped[Optional[str]] = mapped_column(String(128))
     authors: Mapped[str] = mapped_column(String(1024))
     text: Mapped[Optional[str]] = mapped_column(LONGTEXT)
-    confidence: Mapped[Optional[float]] = mapped_column(Float, nullable=True)  # Describes the confidence in how good this article is, as a value <0, 1>
+    confidence: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
     date_published: Mapped[Optional[datetime]]
     meta: Mapped[Optional[JSON]] = mapped_column(JSON, name='metadata', default='{}')
     date_created: Mapped[datetime] = mapped_column(DateTime, default=func.now())
     date_updated: Mapped[Optional[datetime]] = mapped_column(DateTime, onupdate=func.current_timestamp())
-
-    summaries: Mapped[List["Summary"]] = relationship(back_populates="article", cascade="all, delete-orphan")
-    pinecone: Mapped["Pinecone"] = relationship("Pinecone", uselist=False, back_populates="article")
+    
+    pinecone_update_required: Mapped[bool] = mapped_column(Boolean, default=False)
+    pinecone_delete_required: Mapped[bool] = mapped_column(Boolean, default=False)
+    incomplete: Mapped[bool] = mapped_column(Boolean, default=False)
+    
+    summaries: Mapped[List[Summary]] = relationship("Summary", back_populates="article", cascade="all, delete-orphan")
 
     __id_fields = ['title', 'url']
 
@@ -69,29 +60,55 @@ class Article(Base):
         super().__init__(*args, **kwargs)
 
     def __repr__(self) -> str:
-        return f"User(id={self.id!r}, name={self.title!r}, fullname={self.url!r})"
+        return f"Article(id={self.id!r}, name={self.title!r}, fullname={self.url!r}, source={self.source!r}, source_type={self.source_type!r}, authors={self.authors!r}, date_published={self.date_published!r}, pinecone_update_required={self.pinecone_update_required!r}, pinecone_delete_required={self.pinecone_delete_required!r}, incomplete={self.incomplete!r})"
+    
+    def __eq__(self, other):
+        if not isinstance(other, Article):
+            return NotImplemented
+        return (
+            self.id == other.id and
+            self.title == other.title and
+            self.url == other.url and
+            self.source == other.source and
+            self.source_type == other.source_type and
+            self.authors == other.authors and
+            self.date_published == other.date_published and
+            self.text == other.text
+        )
 
     def generate_id_string(self):
         return ''.join(str(getattr(self, field)) for field in self.__id_fields).encode("utf-8")
 
     def verify_fields(self):
         missing = [field for field in self.__id_fields if not getattr(self, field)]
-        assert not missing, f'Entry is missing the following fields: {missing}'
-
-    def verify_id(self):
-        assert self.id is not None, "Entry is missing id"
-
-        id_string = self.generate_id_string()
-        id_from_fields = hashlib.md5(id_string).hexdigest()
-        assert self.id == id_from_fields, f"Entry id {self.id} does not match id from id_fields, {id_from_fields}"
+        if missing:
+            logger.warning(f'Entry is missing the following fields: {missing}')
+            return False
+        return True
 
     @classmethod
     def before_write(cls, mapper, connection, target):
-        target.verify_fields()
+        session = Session(connection)
 
-        if target.id:
-            target.verify_id()
+        # Check if an Article with the same id already exists
+        db_article = session.query(Article).filter(Article.id == target.id).one_or_none()
+        if db_article is not None:
+            # Compare fields and update if necessary
+            for field in ['title', 'url', 'source', 'source_type', 'authors', 'text', 'date_published', 'meta']:
+                if getattr(db_article, field) != getattr(target, field):
+                    setattr(db_article, field, getattr(target, field))
+                    db_article.pinecone_update_required = True
+            return
+
+        # Verify required fields
+        if target.verify_fields():
+            target.incomplete = False
+            target.pinecone_update_required = True
         else:
+            target.incomplete = True
+
+        # Generate id if necessary
+        if target.id is None:
             id_string = target.generate_id_string()
             target.id = hashlib.md5(id_string).hexdigest()
 
