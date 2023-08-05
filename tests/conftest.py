69138bf484ab020d600c94f592d7ef415363771b
Henri Lemoine <henri123lemoine@gmail.com>
2023-08-05 01:05:48 -0400

    fixed conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
 from unittest.mock import patch, Mock
 import pytest
-from align_data.common.alignment_dataset import make_session
+from align_data.common.alignment_dataset import MySQLDB
 
 
 @pytest.fixture(autouse=True, scope='session')
 def mock_db():
     # This just mocks out all db calls, nothing more
-    with patch('align_data.common.alignment_dataset.make_session'):
+    with patch('align_data.common.alignment_dataset.MySQLDB'):
         yield
