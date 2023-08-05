d6a5cd3305d0d532a5ed44318b490bb9ecc52b5d
Henri Lemoine <henri123lemoine@gmail.com>
2023-08-05 02:14:56 -0400

    added stream_pinecone_updates to session

--- align_data/db/session.py
+++ align_data/db/session.py
@@ -1,10 +1,11 @@
-from typing import List, Generator
+from typing import List
 import logging
 
 from contextlib import contextmanager
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from align_data.settings import DB_CONNECTION_URI
+from align_data.db.models import Article
 
 
 logger = logging.getLogger(__name__)
@@ -17,15 +18,11 @@ def make_session(auto_commit=False):
         yield session
         if auto_commit:
             session.commit()
-        except Exception as e:
-            session.rollback()
-            logging.error(f"An error occurred: {str(e)}")
-            raise
-        finally:
-            session.close()
-            
-    def stream_pinecone_updates(self, custom_sources: List[str]):
-        """Yield Pinecone entries that require an update."""
-        query = self.Session.query(Article).filter(Article.pinecone_update_required.is_(True))
+
+
+def stream_pinecone_updates(custom_sources: List[str]):
+    """Yield Pinecone entries that require an update."""
+    with make_session() as session:
+        query = session.query(Article).filter(Article.pinecone_update_required.is_(True))
         query = query.filter(Article.source.in_(custom_sources))
         yield from query.yield_per(1000)
