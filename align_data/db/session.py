76c6ff50bc366dfe10dcd1babd3f221a918321b9
Daniel O'Connell <github@ahiru.pl>
2023-08-17 18:57:24 +0200

    pr fixes

--- align_data/db/session.py
+++ align_data/db/session.py
@@ -22,10 +22,11 @@ def make_session(auto_commit=False):
 
 def stream_pinecone_updates(session, custom_sources: List[str]):
     """Yield Pinecone entries that require an update."""
-    yield from session.query(Article).filter(
-        Article.pinecone_update_required.is_(True),
-    ).filter(
-        Article.is_valid
-    ).filter(
-        Article.source.in_(custom_sources)
-    ).yield_per(1000)
+    yield from (
+        session
+        .query(Article)
+        .filter(Article.pinecone_update_required.is_(True))
+        .filter(Article.is_valid)
+        .filter(Article.source.in_(custom_sources))
+        .yield_per(1000)
+    )
