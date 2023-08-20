8e4e945acc57ffb1d8ec777584224dfbe7530560
Daniel O'Connell <github@ahiru.pl>
2023-08-20 23:10:49 +0200

    filter out items with low confidence

--- align_data/db/session.py
+++ align_data/db/session.py
@@ -2,9 +2,9 @@ from typing import List
 import logging
 
 from contextlib import contextmanager
-from sqlalchemy import create_engine, and_
+from sqlalchemy import create_engine, or_
 from sqlalchemy.orm import Session
-from align_data.settings import DB_CONNECTION_URI
+from align_data.settings import DB_CONNECTION_URI, MIN_CONFIDENCE
 from align_data.db.models import Article
 
 
@@ -28,5 +28,6 @@ def stream_pinecone_updates(session, custom_sources: List[str]):
         .filter(Article.pinecone_update_required.is_(True))
         .filter(Article.is_valid)
         .filter(Article.source.in_(custom_sources))
+        .filter(or_(Article.confidence == None, Article.confidence > MIN_CONFIDENCE))
         .yield_per(1000)
     )
