db6a52decb77084cb573c2043946581b50814584
Henri Lemoine <henri123lemoine@gmail.com>
2023-08-22 06:26:17 -0400

    updated utils

--- align_data/common/utils.py
+++ align_data/common/utils.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List, Tuple, Dict, Union, Any, Optional
+from typing import List, Tuple, Dict, Any, Optional
 from functools import wraps
 
 import openai
@@ -31,9 +31,18 @@ from align_data.settings import (
     DEVICE,
 )
 
+# --------------------
+# CONSTANTS & CONFIGURATION
+# --------------------
+
 logger = logging.getLogger(__name__)
 
-# Configuration for embeddings
+RETRY_CONDITIONS = (
+    retry_if_exception_type(RateLimitError)
+    | retry_if_exception_type(APIError)
+    | retry_if_exception(lambda e: "502" in str(e))
+)
+
 hf_embeddings = (
     HuggingFaceEmbeddings(
         model_name=SENTENCE_TRANSFORMER_EMBEDDINGS_MODEL,
@@ -44,22 +53,20 @@ hf_embeddings = (
     else None
 )
 
-# Type definitions
-EmbeddingType = Optional[List[float]]  # Represents a single embedding or None
-EmbeddingsListType = Optional[List[EmbeddingType]]  # List of embeddings
-ModerationInfoListType = List[Dict[str, Any]]  # Moderation results for all input texts
-GetEmbeddingsReturnType = Union[
-    EmbeddingsListType, Tuple[EmbeddingsListType, ModerationInfoListType]
-]  # Return type for main function
+# --------------------
+# TYPE DEFINITIONS
+# --------------------
 
-RETRY_CONDITIONS = (
-    retry_if_exception_type(RateLimitError)
-    | retry_if_exception_type(APIError)
-    | retry_if_exception(lambda e: "502" in str(e))
-)
+ModerationInfoType = Dict[str, Any]
+
+# --------------------
+# DECORATORS
+# --------------------
 
 
 def handle_openai_errors(func):
+    """Decorator to handle OpenAI-specific exceptions with retries."""
+
     @wraps(func)
     @retry(
         wait=wait_random_exponential(multiplier=1, min=2, max=30),
@@ -96,107 +103,31 @@ def handle_openai_errors(func):
     return wrapper
 
 
-@handle_openai_errors
-def moderation_check(texts):
-    return openai.Moderation.create(input=texts)["results"]
-
-
-@handle_openai_errors
-def compute_openai_embeddings(non_flagged_texts, engine, **kwargs):
-    data = openai.Embedding.create(input=non_flagged_texts, engine=engine, **kwargs).data
-    return [d["embedding"] for d in data]
-
-
-def get_embeddings(
-    texts: List[str],
-    embed_all: bool = False,
-    return_moderation_info: bool = False,
-    source: str = "no source",
-    engine=OPENAI_EMBEDDINGS_MODEL,
-    **kwargs,
-) -> GetEmbeddingsReturnType:
-    assert len(texts) <= 2048, "The batch size should not be larger than 2048."
-    assert all(texts), "No empty strings allowed in the input list."
-
-    # Step 1: Check all texts for moderation flags
-    moderation_results = moderation_check(texts)
-    flagged_bools = [result["flagged"] for result in moderation_results]
-
-    # If not embedding all and any text is flagged, return None immediately
-    if not embed_all and any(flagged_bools):
-        if return_moderation_info:
-            return None, moderation_results
-        return None
-
-    # Filter out flagged texts before embedding and replace newlines, which can negatively affect performance.
-    non_flagged_texts = [text for text, flagged in zip(texts, flagged_bools) if not flagged]
-    non_flagged_texts = [text.replace("\n", " ") for text in non_flagged_texts]
-
-    # Step 2: Compute embeddings for non-flagged texts
-    non_flagged_embeddings = []
-    if non_flagged_texts:  # Only call the embedding function if there are non-flagged texts
-        if USE_OPENAI_EMBEDDINGS:
-            non_flagged_embeddings = compute_openai_embeddings(non_flagged_texts, engine, **kwargs)
-        else:
-            non_flagged_embeddings = hf_embeddings.embed_documents(
-                non_flagged_texts
-            )  # Assuming this doesn't require the same error handling
-
-    # Bias adjustment
-    if bias := EMBEDDING_LENGTH_BIAS.get(source, 1.0):
-        non_flagged_embeddings = [
-            [bias * e for e in embedding] for embedding in non_flagged_embeddings
-        ]
-
-    # Step 3: Reconstruct the final list of embeddings with None for flagged texts
-    non_flagged_iter = iter(non_flagged_embeddings)
-    final_embeddings = [None if flagged else next(non_flagged_iter) for flagged in flagged_bools]
-
-    if return_moderation_info:
-        return final_embeddings, moderation_results
-    return final_embeddings
-
-
-def get_embedding(text: str, **kwargs) -> List[float]:
-    return get_embeddings(texts=[text], **kwargs)[0]
+# --------------------
+# UTILITY FUNCTIONS
+# --------------------
 
 
 def get_recursive_type(obj):
+    """Get the nested type of a given object."""
+
     if isinstance(obj, list):
-        # If the list is empty, just return List[Any]
-        if not obj:
-            return "List[Any]"
-        # Recursively get the type of the first item in the list
-        # This assumes that all items in the list are of the same type
-        return f"List[{get_recursive_type(obj[0])}]"
-
+        return f"List[{get_recursive_type(obj[0]) if obj else 'Any'}]"
     elif isinstance(obj, tuple):
-        # Get the type of each item in the tuple
-        types = ", ".join(get_recursive_type(item) for item in obj)
-        return f"Tuple[{types}]"
-
+        return f"Tuple[{', '.join(get_recursive_type(item) for item in obj)}]"
     elif isinstance(obj, dict):
-        # If the dict is empty, just return Dict[Any, Any]
-        if not obj:
-            return "Dict[Any, Any]"
-        # Get the type of the first key and the first value in the dict
-        # This assumes that all keys are of the same type and all values are of the same type
-        key_type = get_recursive_type(next(iter(obj.keys())))
-        value_type = get_recursive_type(next(iter(obj.values())))
-        return f"Dict[{key_type}, {value_type}]"
-
+        keys, values = next(iter(obj.items())) if obj else (Any, Any)
+        return f"Dict[{get_recursive_type(keys)}, {get_recursive_type(values)}]"
     else:
         return type(obj).__name__
 
 
 def get_recursive_length(obj):
+    """Determine the nested length of a given object."""
     lengths = []
 
-    # Base case: if the object is a string or number, return its length or 1
     if isinstance(obj, (str, int, float)):
         return len(obj) if isinstance(obj, str) else 1
-
-    # If it's a list or tuple, get the length of the current level
     if isinstance(obj, (list, tuple)):
         lengths.append(len(obj))
         sub_lengths = [get_recursive_length(item) for item in obj]
@@ -208,8 +139,6 @@ def get_recursive_length(obj):
                 [sub[i] if isinstance(sub, list) and len(sub) > i else 1 for sub in sub_lengths]
             )
         return lengths
-
-    # If it's a dictionary, get the length of the current level and then of each key and value
     if isinstance(obj, dict):
         lengths.append(len(obj))
         keys_lengths = [get_recursive_length(k) for k in obj.keys()]
@@ -227,6 +156,125 @@ def get_recursive_length(obj):
                 ]
             )
         return lengths
-
-    # For other types, just return 1
     return 1
+
+
+# --------------------
+# MAIN FUNCTIONS
+# --------------------
+
+
+@handle_openai_errors
+def moderation_check(texts):
+    return openai.Moderation.create(input=texts)["results"]
+
+
+@handle_openai_errors
+def compute_openai_embeddings(non_flagged_texts, engine, **kwargs):
+    data = openai.Embedding.create(input=non_flagged_texts, engine=engine, **kwargs).data
+    return [d["embedding"] for d in data]
+
+
+def get_embeddings_without_moderation(
+    texts: List[str],
+    source: str = "no source",
+    engine=OPENAI_EMBEDDINGS_MODEL,
+    **kwargs,
+) -> List[List[float]]:
+    """
+    Obtain embeddings without moderation checks.
+
+    Parameters:
+    - texts (List[str]): List of texts to be embedded.
+    - source (str, optional): Source identifier to potentially adjust embedding bias. Defaults to "no source".
+    - engine (str, optional): Embedding engine to use (relevant for OpenAI). Defaults to OPENAI_EMBEDDINGS_MODEL.
+    - **kwargs: Additional keyword arguments passed to the embedding function.
+
+    Returns:
+    - List[List[float]]: List of embeddings for the provided texts.
+    """
+
+    embeddings = []
+    if texts:  # Only call the embedding function if there are non-flagged texts
+        if USE_OPENAI_EMBEDDINGS:
+            embeddings = compute_openai_embeddings(texts, engine, **kwargs)
+        else:
+            embeddings = hf_embeddings.embed_documents(texts)
+
+    # Bias adjustment
+    if bias := EMBEDDING_LENGTH_BIAS.get(source, 1.0):
+        embeddings = [[bias * e for e in embedding] for embedding in embeddings]
+
+    return embeddings
+
+
+def get_embeddings_or_none_if_flagged(
+    texts: List[str],
+    source: str = "no source",
+    engine=OPENAI_EMBEDDINGS_MODEL,
+    **kwargs,
+) -> Tuple[Optional[List[List[float]]], List[ModerationInfoType]]:
+    """
+    Obtain embeddings for the provided texts. If any text is flagged during moderation,
+    the function returns None for the embeddings while still providing the moderation results.
+
+    Parameters:
+    - texts (List[str]): List of texts to be embedded.
+
+    Returns:
+    - Tuple[Optional[List[List[float]]], ModerationInfoListType]: Tuple containing the list of embeddings (or None if any text is flagged) and the moderation results.
+    """
+    moderation_results = moderation_check(texts)
+    if any(result["flagged"] for result in moderation_results):
+        return None, moderation_results
+
+    embeddings = get_embeddings_without_moderation(texts, source, engine, **kwargs)
+    return embeddings, moderation_results
+
+
+def get_embeddings(
+    texts: List[str],
+    source: str = "no source",
+    engine=OPENAI_EMBEDDINGS_MODEL,
+    **kwargs,
+) -> Tuple[List[Optional[List[float]]], List[ModerationInfoType]]:
+    """
+    Obtain embeddings for the provided texts, replacing the embeddings of flagged texts with `None`.
+
+    Parameters:
+    - texts (List[str]): List of texts to be embedded.
+    - source (str, optional): Source identifier to potentially adjust embedding bias. Defaults to "no source".
+    - engine (str, optional): Embedding engine to use (relevant for OpenAI). Defaults to OPENAI_EMBEDDINGS_MODEL.
+    - **kwargs: Additional keyword arguments passed to the embedding function.
+
+    Returns:
+    - Tuple[List[Optional[List[float]]], ModerationInfoListType]: Tuple containing the list of embeddings (with None for flagged texts) and the moderation results.
+    """
+    assert len(texts) <= 2048, "The batch size should not be larger than 2048."
+    assert all(texts), "No empty strings allowed in the input list."
+
+    # replace newlines, which can negatively affect performance
+    texts = [text.replace("\n", " ") for text in texts]
+
+    # Check all texts for moderation flags
+    moderation_results = moderation_check(texts)
+    flagged_bools = [result["flagged"] for result in moderation_results]
+
+    non_flagged_texts = [text for text, flagged in zip(texts, flagged_bools) if not flagged]
+    non_flagged_embeddings = get_embeddings_without_moderation(
+        non_flagged_texts, source, engine, **kwargs
+    )
+
+    # Reconstruct the final list of embeddings with None for flagged texts
+    embeddings_iter = iter(non_flagged_embeddings)
+    embeddings = [None if flagged else next(embeddings_iter) for flagged in flagged_bools]
+
+    return embeddings, moderation_results
+
+
+def get_embedding(
+    text: str, source: str = "no source", engine=OPENAI_EMBEDDINGS_MODEL, **kwargs
+) -> Tuple[List[float], ModerationInfoType]:
+    """Obtain an embedding for a single text."""
+    embedding, moderation_result = get_embeddings([text], source=source, engine=engine, **kwargs)
+    return embedding[0], moderation_result[0]
