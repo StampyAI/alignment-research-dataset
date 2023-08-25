9b6b2529d42193b95d6eb149d62013f679a62253
Henri Lemoine <henri123lemoine@gmail.com>
2023-08-25 02:16:19 -0400

    pinecone_update_required bug fix

--- align_data/common/alignment_dataset.py
+++ align_data/common/alignment_dataset.py
@@ -74,7 +74,6 @@ class AlignmentDataset:
         authors = data.pop("authors", [])
 
         article = Article(
-            pinecone_update_required=True,
             meta={k: v for k, v in data.items() if k not in ARTICLE_MAIN_KEYS and v is not None},
             **{k: v for k, v in data.items() if k in ARTICLE_MAIN_KEYS},
         )
