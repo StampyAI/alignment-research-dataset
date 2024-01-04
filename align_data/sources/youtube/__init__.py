8140ad1ab0ea2b3d9c7d522bd5b6c7341d4af744
markov <52564817+markovial@users.noreply.github.com>
2024-01-04 22:12:25 +0000

    added youtube channels

--- align_data/sources/youtube/__init__.py
+++ align_data/sources/youtube/__init__.py
@@ -30,15 +30,45 @@ YOUTUBE_DATASETS = [
         channel_id="UCNJ1Ymd5yFuUPtn21xtRbbw",
         authors=[],
     ),
+    YouTubeChannelDataset(
+        name="machine_intelligence_research_institute",
+        channel_id="UCutigUZUpKrIMOte27SDmEg",
+        authors=[],
+    ),
+    YouTubeChannelDataset(
+        name="agent_foundations_for_ai_alignment_workshop",
+        channel_id="UCWYnqb7XeahrMdUvW56TJ1Q",
+        authors=[],
+    ),
+    YouTubeChannelDataset(
+        name="neel_nanda",
+        channel_id="UCBMJ0D-omcRay8dh4QT0doQ",
+        authors=["Neel Nanda"],
+    ),
+    YouTubeChannelDataset(
+        name="future_of_life_institute",
+        channel_id="UC-rCCy3FQ-GItDimSR9lhzw",
+        authors=[],
+    ),
+    YouTubeChannelDataset(
+        name="the_inside_view",
+        channel_id="UCb9F9_uV24PGj6x63PhXEVw",
+        authors=[],
+    ),
+    YouTubeChannelDataset(
+        name="center_for_ai_safety",
+        channel_id="UCY_K5gXsXHtuiP8mj3BiWxA",
+        authors=["Dan Hendrycks"],
+    ),
     YouTubePlaylistDataset(
         name="ai_alignment_playlist",
         playlist_ids=[
             "PLqYmG7hTraZCRwoyGxvQkqVrZgDQi4m-5",
             "PLqYmG7hTraZBiUr6_Qf8YTS2Oqy3OGZEj",
-            "PLAPVC5uNprwY0q4_nyeeHqIT07wZqwjGO",
             "PLCRVRLd2RhZTpdUdEzJjo3qhmX3y3skWA",
             "PLTYHZYmxohXpn5uf8JZ2OouB1PsDJAk-x",
             "PLWQikawCP4UEiXxzfA8CfBsgDeXzXt9s9",
+            "PLoROMvodv4rNtnS3JSRRZzLWQo2dd6XNs",
         ],
     ),
 ]
